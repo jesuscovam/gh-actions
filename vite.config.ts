a789a0216d6d816603d94be3227e0be6f6a47279
Jesus Cova <jesuscovam@gmail.com>
2022-06-08 12:02:10 -0500

    nuevo url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "https://ghactions.jesuscova.com",
+	base: "https://jesuscovam.github.io/gh-actions/",
 	plugins: [react()],
 })
